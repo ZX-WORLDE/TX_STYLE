ca8f3bec88ceb335909f388a831bdaaa7e6b67cb
ZX-SHADHIN <124778109+ZX-WORLDE@users.noreply.github.com>
2023-06-04 15:45:41 +0600

    Update TX_SET-UP.py

--- TX_SET-UP.py
+++ TX_SET-UP.py
@@ -89,9 +89,9 @@ def zx():
 
         if internet_connection:
             jalan("       \033[1;96m  [\033[1;92mSET UP YOUR SYSTEM \033[1;91mDON'T \033[1;92mSKIP\033[1;96m]")
-            print("\033[1;30m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+            print("\033[1;30m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━033[1;32m")
             os.system("cd tarmux && bash setup.sh")
-            with open("sarm.txt", "w") as file:
+            with open("sarv.txt", "w") as file:
                 file.write("FILE")
             TX_SL()
         else:
@@ -112,7 +112,7 @@ def zx():
             if zx in ["1", "01"]:
                 os.system("clear")
                 jalan("       \033[1;96m  [\033[1;92mSET UP YOUR SYSTEM \033[1;91mDON'T \033[1;92mSKIP\033[1;96m]")
-                print("\033[1;30m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+                print("\033[1;30m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━033[1;32m")
                 os.system("cd tarmux && bash setup.sh")
             elif zx in ["2", "02"]:
                 time.sleep(1)
